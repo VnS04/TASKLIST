cd09d252f5b7598d6d314b7c8793915cbface01f
VnS04 <56930971+VnS04@users.noreply.github.com>
2021-08-24 18:27:17 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const btnAdicionar = document.querySelector("#addTask");
+
+function createElement(text) {
+  const lista = document.getElementById("todo-list ul");
+  const li = document.createElement("li");
+  li.innerHTML = `<input type='checkbox'>` + `<label>${text}</lable>`;
+  lista.appendChild(li);
+}
+
+btnAdicionar.addEventListener("click", (event) => {
+  const tarefa = document.querySelector("#new-task");
+  if (tarefa.value != "") createElement(tarefa.value);
+});
